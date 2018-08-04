50a3a9d9a006fb95dc20fbe880fbdcebadd9aea6
GamingAuthor <league@WTS20.attlocal.net>
2018-08-04 11:28:18 -0700

    Module 4!
    is done!

--- src/threads/BruteForceCracker.java
+++ src/threads/BruteForceCracker.java
@@ -5,6 +5,8 @@ package threads;
 // to find the code to be as little as possible.
 // Let's see who can crack the code the fastest!
 
+import java.net.StandardSocketOptions;
+
 // Pro-tip: Do not include the creation of the threads as part of the time. 
 
 import java.util.Random;
@@ -14,42 +16,76 @@ public class BruteForceCracker {
 	static long startTime;
 	static long endTime;
 	static float elapsedTime;
+	static long finale = 0;
 	
 	public static void main(String[] args) {
-		int ctr = 0;
-		Long.toString(code);
-		code+=4;
-		System.out.println(code);
-		Thread brute = new Thread(()->{
-			for(int i = 0; i <= 9; i++) {
-				
-			}
+		String s = 	Long.toString(code);
+		Thread one = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(0)));
+			finale += c*=100000000;
+			System.out.println(finale);
 			});
-		Thread force = new Thread(()->{
-			for(int i = 0; i <= 9; i++) {
-
-			}
+		Thread two = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(1)));
+			finale += c*=10000000;
+			System.out.println(finale);
 			});
-		Thread cracker = new Thread(()->{
-			for(int i = 0; i <= 9; i++) {
-
-			}
+		Thread three = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(2)));
+			finale += c*=1000000;
+			System.out.println(finale);
 			});
+		Thread four = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(3)));
+			finale += c*=100000;
+			System.out.println(finale);
+			});
+		Thread five = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(4)));
+			finale += c*=10000;
+			System.out.println(finale);
+			});
+		Thread six = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(5)));
+			finale += c*=1000;
+			System.out.println(finale);
+			});
+		Thread seven = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(6)));
+			finale += c*=100;
+			System.out.println(finale);
+			});
+		Thread eight = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(7)));
+			finale += c*=10;
+			System.out.println(finale);
+			});
+		Thread nine = new Thread(()->{
+			long c = Integer.parseInt(Character.toString(s.charAt(8)));
+			finale += c*=1;
+			System.out.println(finale);
+			});
+		
 		System.out.println("Starting Brute Force Checker");
 		System.out.println(code);
 		startTime = System.currentTimeMillis();
-		while(!checkCode(ctr)) {
-			for(int i = 0; i <=code; i++) {
-				ctr=i;
-			}
-			System.out.println(checkCode(ctr));
+		while(!checkCode(finale)) {
+			one.run();
+			two.run();
+			three.run();
+			four.run();
+			five.run();
+			six.run();
+			seven.run();
+			eight.run();
+			nine.run();
 		}
 		endTime = System.currentTimeMillis();
 		elapsedTime = (float)(endTime - startTime);
 		elapsedTime /= 1000.f;
 		System.out.println("Total time taken: " + elapsedTime + " seconds");
 	}
-	
+
 	public static boolean checkCode(long p){
 		if(p == code){
 			return true;
@@ -57,11 +93,5 @@ public class BruteForceCracker {
 			return false;
 		}
 	}
-	public static void variable(long ree) {
-		if(ree == code) {
-			
-		} else {
-		ree++; 
-		}
-	}
 }
+
